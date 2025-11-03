41d4466c573d4e97e6dd9a0b2f54f4bfef4f1774
lofi boy <lofiboyog@gmail.com>
2025-11-03 21:27:36 +0000

    also add this code in every head of every page:
    <!-- Google AdSense Aut

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,9 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
+        <script async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3042243846300811"
+          crossOrigin="anonymous"></script>
       </head>
       <body className="font-body antialiased" suppressHydrationWarning>
         {children}
