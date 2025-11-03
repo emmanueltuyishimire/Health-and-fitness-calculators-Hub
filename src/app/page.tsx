984ec898b068e383409eb7d06cac7e309c83b36f
lofi boy <lofiboyog@gmail.com>
2025-11-03 22:20:19 +0000

    show that copyright owner is a link by using special color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -118,7 +118,7 @@ export default function Home() {
       </main>
 
       <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t">
-        <p className="text-xs text-muted-foreground">&copy; 2025 <a href="https://health.calculation.site" target="_blank" rel="noopener noreferrer" className="hover:underline">calculation.site</a>. All rights reserved.</p>
+        <p className="text-xs text-muted-foreground">&copy; 2025 <a href="https://calculation.site" target="_blank" rel="noopener noreferrer" className="text-primary hover:underline">calculation.site</a>. All rights reserved.</p>
         <nav className="sm:ml-auto flex flex-wrap gap-4 sm:gap-6">
           <a className="text-xs hover:underline underline-offset-4" href="https://calculation.site/about" target="_blank" rel="nofollow noopener noreferrer">
             About
