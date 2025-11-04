fbe403ab50b807d62b1e876db03df0e43db44e65
lofi boy <lofiboyog@gmail.com>
2025-11-04 00:00:24 +0000

    it is pointing to readme again

--- next.config.ts
+++ next.config.ts
@@ -32,9 +32,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  allowedDevOrigins: [
-    '6000-firebase-studio-1762192148188.cluster-ikslh4rdsnbqsvu5nw3v4dqjj2.cloudworkstations.dev',
-  ],
 };
 
 export default nextConfig;
