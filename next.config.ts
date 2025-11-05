7f9d827672daea9db63b18d160cec0ae1966755c
lofi boy <lofiboyog@gmail.com>
2025-11-05 21:35:10 +0000

    how about repo name and user name

--- next.config.ts
+++ next.config.ts
@@ -12,11 +12,6 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  devIndicators: {
-    allowedDevOrigins: [
-      '6000-firebase-studio-1762192148188.cluster-ikslh4rdsnbqsvu5nw3v4dqjj2.cloudworkstations.dev',
-    ],
-  },
 };
 
 export default nextConfig;
