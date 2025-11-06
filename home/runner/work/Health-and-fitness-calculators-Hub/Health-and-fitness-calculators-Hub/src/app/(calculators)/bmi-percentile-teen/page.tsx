4b789ccb0fdb598f16310b432deb275d3f484053
lofi boy <lofiboyog@gmail.com>
2025-11-06 01:08:02 +0000

    Run npx --no-install next build ⚠ No build cache found. Please configure

--- home/runner/work/Health-and-fitness-calculators-Hub/Health-and-fitness-calculators-Hub/src/app/(calculators)/bmi-percentile-teen/page.tsx
+++ home/runner/work/Health-and-fitness-calculators-Hub/Health-and-fitness-calculators-Hub/src/app/(calculators)/bmi-percentile-teen/page.tsx
@@ -25,7 +25,7 @@ import {
   TableHeader,
   TableRow,
 } from '@/components/ui/table';
-import { BmiPercentileTeenCalculatorForm } from '@/components/bmi-percentile-teen-calculator-form';
+import { BmiPercentileTeenCalculatorForm } from '@/components/BmiPercentileTeenCalculatorForm';
 
 export const metadata: Metadata = {
   title: 'BMI Percentile Calculator for Teens & Children',
